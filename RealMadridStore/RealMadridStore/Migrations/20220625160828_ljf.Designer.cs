3ef96b63eb917e1747792aeeecf8751f8dac052d
MutazTB <mutazaltbakhi900@gmail.com>
2022-06-25 21:12:08 +0300

    ignor

--- RealMadridStore/RealMadridStore/Migrations/20220625160828_ljf.Designer.cs
+++ RealMadridStore/RealMadridStore/Migrations/20220625160828_ljf.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealMadridStore.Data;
 
 namespace RealMadridStore.Migrations
 {
     [DbContext(typeof(RealMadridDBContext))]
-    partial class RealMadridDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220625160828_ljf")]
+    partial class ljf
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
